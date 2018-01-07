44b7e41b25b77b375bbcf3dd312515a3b3463cdf
Egorivnv <esiv313@yandex.ru>
2018-01-07 13:38:57 +0300

    Finished 1st chapter

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -96,6 +96,8 @@ namespace SportsStore
                 });
 
             SeedData.EnsurePopulated(app);
+            IdentitySeedData.EnsurePopulated(app);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
